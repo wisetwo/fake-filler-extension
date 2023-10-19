d8b62891dc066e086b5af4b752c7acb0d80c4aeb
Jim Hays <calvinballing@gmail.com>
2023-10-19 18:40:50 -0400

    fixup: remove isproenabled

--- src/options/index.tsx
+++ src/options/index.tsx
@@ -9,17 +9,12 @@ import App from "src/options/components/App";
 import Store from "src/options/store";
 import { FirebaseUser, FirebaseCustomClaims, IFakeFillerOptions } from "src/types";
 
-let isProEnabled = false;
-
 function handleAuthStateChange(user: FirebaseUser, claims: FirebaseCustomClaims) {
-  isProEnabled = claims ? claims.subscribed : false;
   Store.dispatch(updateAuthState(user, claims));
 }
 
 async function handleOptionsChange(options: IFakeFillerOptions) {
-  if (isProEnabled) {
-    Store.dispatch({ type: "RECEIVED_OPTIONS", options });
-  }
+  Store.dispatch({ type: "RECEIVED_OPTIONS", options });
 }
 
 onAuthStateChange(handleAuthStateChange);
